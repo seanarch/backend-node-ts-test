6197c6731ad59bff1f4c33effee4cd16b9381a53
seanarch <zhuqingyun@outlook.com>
2023-05-10 17:18:37 -0600

    Setup mongo connection in env

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 const dotenv = require("dotenv").config({
-  path: path.resolve(__dirname + "/.env"),
+  path: path.resolve(__dirname, "../.env"),
 });
 
 const app = express();
 
 const MONGO_URL = process.env.MONGO_URI;
-console.log(MONGO_URL);
+
+mongoose.connect(MONGO_URL);
+const db = mongoose.connection;
+db.on("error", (error: string) => console.error(error));
+db.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+app.use(express.json());
 
 app.listen(3001, () => console.log("Listening on port 3001"));
 console.log(new Date());
