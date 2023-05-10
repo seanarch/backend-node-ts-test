6ca12f61e5a7dcf6ce467229019b70a9e34f4ea4
seanarch <zhuqingyun@outlook.com>
2023-05-10 17:47:01 -0600

    Added model

---
+++ src/routes/planets.router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const Planet = require("../models/planet.model");
+
+// GET
+
+// GET one
+
+// Add one
+
+// Update one
+
+// Delete one
