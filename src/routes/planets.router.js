89315cff8266da8f0f93843a3db3c73ebd2d9815
seanarch <zhuqingyun@outlook.com>
2023-05-10 17:49:35 -0600

    Get all method

--- src/routes/planets.router.js
+++ src/routes/planets.router.js
@@ -3,11 +3,23 @@ const router = express.Router();
 const Planet = require("../models/planet.model");
 
 // GET
+router.get("/", async (req, res) => {
+  try {
+    const planets = await Planet.find();
+    res.json(planets);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // GET one
+router.get("/", async (req, res) => {});
 
 // Add one
+router.post("/", async (req, res) => {});
 
 // Update one
+router.patch("/", async (req, res) => {});
 
 // Delete one
+router.delete("/", async (req, res) => {});
