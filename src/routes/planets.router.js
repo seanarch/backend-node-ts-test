4e60d2026c00026792a4732190f61ede98ba34a4
seanarch <zhuqingyun@outlook.com>
2023-05-10 21:33:10 -0600

    Added patch and delete method

--- src/routes/planets.router.js
+++ src/routes/planets.router.js
@@ -13,21 +13,46 @@ router.get("/", async (req, res) => {
 });
 
 // GET one
-router.get("/", async (req, res) => {
+router.get("/:id", getPlanet, async (req, res) => {
+  res.json(res.planet);
+});
+
+// Add one
+router.post("/", async (req, res) => {
+  const planet = new Planet({
+    keplerName: req.body.keplerName,
+  });
+
   try {
+    const newPlanet = await planet.save();
+    res.status(201).json(newPlanet);
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
 
-// Add one
-router.post("/", async (req, res) => {});
-
 // Update one
-router.patch("/", async (req, res) => {});
+router.patch("/:id", getPlanet, async (req, res) => {
+  if (req.body.keplerName !== null) {
+    res.planet.keplerName = req.body.keplerName;
+  }
+  try {
+    const updatedPlanet = await res.planet.save();
+    res.json(updatedPlanet);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Delete one
-router.delete("/", async (req, res) => {});
+router.delete("/:id", getPlanet, async (req, res) => {
+  try {
+    await res.planet.deleteOne();
+    res.json({ message: "Deleted Planet" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 async function getPlanet(req, res, next) {
   let planet;
